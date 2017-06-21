d18821483dfebac2e56d0072275ddfeb36ead9c3
ctab33 <ctab33@me.com>
2017-06-21 19:27:14 -0400

    beginning scaper file

--- lib/current_events_explained/cli.rb
+++ lib/current_events_explained/cli.rb
@@ -1,7 +1,32 @@
 class CurrentEventsExplained::CLI
 
   def call
-    puts "Hello World"
+    explainer_list
+    user_prompt
+    user_done
   end
+
+  def explainer_list
+    puts "Choose from Vox's list of explainers."
+    puts <<-DOC.gsub /^\s*/, ''
+      1. Trump’s new policy to tackle sky-high drug prices makes sense — sort of
+      2. The outrage over Gal Gadot’s $300,000 paycheck for Wonder Woman, explained
+    DOC
+  end
+
+  def user_prompt
+    puts "What explainer would you like to read? Please enter a number."
+    input = gets.strip
+    #Rest of code goes here. Could be case, loop, etc to show when user picks a number, it shows them article.
+    #Don't forget to include an exit strategy for user.
+    #Account for numbers that aren't listed. 
+  end
+
+  def user_done
+    puts "Thanks for stopping by."
+  end
+
+
+
 
 end
---
+++ lib/current_events_explained/scraper.rb
@@ -0,0 +1,15 @@
+class CurrentEventsExplained::Scraper
+  attr_accessor :title, :author, :date, :url
+
+    def self.scrape_explainer
+      doc = Nokogiri::HTML(open('https://www.vox.com/explainers'))
+      articles = doc.search()
+
+    end
+end
+
+
+#The scaper should tenatively do the following
+# 1. Iterate over the articles
+# 2. Return a numbered array with title, author, date, and url. Use flatten?
+# 3.
