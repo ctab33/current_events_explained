981acd2aa771f1564c3b85feceec4e2775e133f4
ctab33 <ctab33@me.com>
2017-06-22 10:24:44 -0400

    troublshooting constant

--- lib/current_events_explained/scraper.rb
+++ lib/current_events_explained/scraper.rb
@@ -1,8 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 class CurrentEventsExplained::Scraper
   attr_accessor :title, :author, :date, :url
 
     def self.scrape_explainer
       doc = Nokogiri::HTML(open('https://www.vox.com/explainers'))
+      binding.pry
       articles = doc.search()
 
     end
