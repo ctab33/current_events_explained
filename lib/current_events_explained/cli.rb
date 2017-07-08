c3a5fdb17fed33f42cd88bf628427d414e9d8f23
ctab33 <ctab33@me.com>
2017-07-07 20:34:21 -0400

    attempt at finishing cli but it's in currently in an infinite loop

--- lib/current_events_explained/cli.rb
+++ lib/current_events_explained/cli.rb
@@ -1,7 +1,7 @@
 class CurrentEventsExplained::CLI
 
   def call
-    explainer_list
+    #explainer_list
     user_prompt
     #user_done
   end
@@ -14,50 +14,50 @@ class CurrentEventsExplained::CLI
       puts "  #{i}. #{articles.title}"
     end
     2.times {puts ""}
+    user_instructions
+  end
+
+  def user_instructions
+    puts ""
+    puts "Which explainer would you like more information on? Please enter a corresponding number."
+    puts "If you'd like to see the list again, type in vox."
+    puts "If you're done, type in exit."
+    puts ""
   end
 
   def user_prompt
-      puts "What explainer would you like to information on? Please enter a number."
-      puts "If you'd like to see the list again, type in vox."
-      puts "If you're done, type in exit."
-      puts ""
-      input = gets.strip
-      if input == "vox"
-        explainer_list
-      elsif input.to_i == 0
-        puts "Please enter a number that is either 1 or greater."
-        explainer_list
-      elsif input.to_i > 0
-        if articles = CurrentEventsExplained::Articles.find(input.to_i)
-          article_details(articles)
-          2.times {puts ""}
-          puts "Would you like to choose another article? Enter Y or N."
-          input = gets.strip.downcase
-          if input == "y"
-            explainer_list
-          else
-            puts ""
-            puts "Thanks for stopping by!"
-            exit
-          end
-         end
-      else
-        exit
-      end
+      input = nil
+      while input != "exit"
+        input = gets.strip
+        if input == "vox"
+          explainer_list
+          user_instructions
+        elsif input.to_i == 0
+          explainer_list
+          puts "Please enter a number that is either 1 or greater."
+        else input.to_i > 0
+          if articles = CurrentEventsExplained::Articles.find(input.to_i)
+            article_details(articles)
+            2.times {puts ""}
+            puts "Would you like to choose another article? Enter Y or N."
+            input = gets.strip.downcase
+              if input == "y"
+                explainer_list
+              else
+                puts "Thanks for stopping by!"
+            #exit
+          end #ends line 41
+        end #end line 36
+      end #end line 30
+    end #end until
     end
 
     def article_details(articles)
-      puts ""
       puts "#{articles.title}"
-      puts ""
       puts "Written by: #{articles.author}"
-      puts ""
       puts "Author's Twitter handle (if provided):  #{articles.twitter_handle}"
-      puts ""
       puts "Date and time of publication: #{articles.date_time}"
-      puts ""
       puts "Link: #{articles.url}"
-      puts ""
       puts "Introduction: #{articles.intro}"
     end
 end
